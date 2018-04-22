ea0bc6b9a11e4e1e9aa0d2a1f31a644f9b256ecf
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-21 21:35:50 -0400

    brew

--- init.zsh
+++ init.zsh
@@ -9,3 +9,24 @@ p6df::modules::homebrew::external() {
 p6df::modules::homebreg::init() {
 }
 
+
+## XXX: broken
+remove_casks() {
+
+    local formuli=$(grep ^cask bin/Brewfile | awk '{print $2}' | sed -e "s,',,g")
+
+    local formula
+    for formula in $formuli; do
+        brew cask uninstall $formula
+    done
+}
+
+remove_brews() {
+
+    local formuli=$(grep ^brew bin/Brewfile | awk '{print $2}' | sed -e "s,',,g" -e 's/,.*//')
+
+    local formula
+    for formula in $formuli; do
+        brew uninstall --ignore-dependencies $formula
+    done
+}
