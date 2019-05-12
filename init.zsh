0984f4cefca1b87709501afdf68140b92708b53d
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-05-11 21:27:29 -0400

    unbreak, need api catch up

--- init.zsh
+++ init.zsh
@@ -1,37 +1,28 @@
 p6df::modules::homebrew::version() { echo "0.0.1" }
-p6df::modules::homebrew::deps()    { 
+p6df::modules::homebrew::deps()    {
 	ModuleDeps=()
-}
-
-p6df::modules::homebrew::external::brew() { 
-}
-
-p6df::modules::homebrew::home::symlink() {
-
-  # XXX: none
-  true;
 }
 
 p6df::modules::homebrew::init() {
 }
 
-## XXX: brokedn
+# XXX: API
 remove_casks() {
 
-    local formuli=$(grep ^cask bin/Brewfile | awk '{print $2}' | sed -e "s,',,g")
+    local formuli=$(brew cask list)
 
     local formula
-    for formula in $formuli; do
-        brew cask uninstall $formula
+    for formula in $(echo $formuli); do
+	brew cask uninstall $formula
     done
 }
 
 remove_brews() {
 
-    local formuli=$(grep ^brew bin/Brewfile | awk '{print $2}' | sed -e "s,',,g" -e 's/,.*//')
+    local formuli=$(brew list)
 
     local formula
-    for formula in $formuli; do
-        brew uninstall --ignore-dependencies $formula
+    for formula in $(echo $formuli); do
+	brew uninstall --ignore-dependencies $formula
     done
 }
