b1f804f05747f99c257544c5a9e6cf81cb84e8b5
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-05-27 10:12:34 -0400

    works

--- init.zsh
+++ init.zsh
@@ -1,28 +1,39 @@
 p6df::modules::homebrew::version() { echo "0.0.1" }
-p6df::modules::homebrew::deps()    {
-	ModuleDeps=()
-}
 
+# Warning: Using vim because no editor was set in the environment.
+# This may change in the future, so we recommend setting EDITOR,
+# or HOMEBREW_EDITOR to your preferred text editor.
 p6df::modules::homebrew::init() {
+
+  export HOMEBREW_EDITOR=vim
+
+  alias p6_hbr=p6df::modules::homebrew::remove
+  alias p6_hbcr=p6df::modules::homebrew::casks::remove
+  alias p6_hbbr=p6df::modules::homebrew::brew::remove
 }
 
-# XXX: API
-remove_casks() {
+p6df::modules::homebrew::remove() {
 
-    local formuli=$(brew cask list)
-
-    local formula
-    for formula in $(echo $formuli); do
-	brew cask uninstall $formula
-    done
+  p6df::modules::homebrew::casks::remove
+  p6df::modules::homebrew::brews::remove
 }
 
-remove_brews() {
+p6df::modules::homebrew::casks::remove() {
 
-    local formuli=$(brew list)
+  local formuli=$(brew cask list)
 
-    local formula
-    for formula in $(echo $formuli); do
-	brew uninstall --ignore-dependencies $formula
-    done
+  local formula
+  for formula in $(echo $formuli); do
+    brew cask uninstall $formula
+  done
+}
+
+p6df::modules::homebrew::brews::remove() {
+
+  local formuli=$(brew list)
+
+  local formula
+  for formula in $(echo $formuli); do
+    brew uninstall --ignore-dependencies $formula
+  done
 }
